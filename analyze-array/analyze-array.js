5cf829d0752e4cb180159bac51a98e981a099ba4
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-15 17:30:45 +0800

    feat: use mergesort function to analyze the arr then return object
    that contain the following:
      average -> length / 2
      min -> 0
      max -> length - 1
      length -> length

--- analyze-array/analyze-array.js
+++ analyze-array/analyze-array.js
@@ -1,58 +1,14 @@
-import analyzeArray from "./analyze-array.js";
-
-describe("analyze array returns an object containing min and average and max length with it's value properties", () => {
-  test("works with sorted array", () => {
-    expect(analyzeArray([0, 1, 2, 3, 4])).toEqual({
-      average: 2,
-      min: 0,
-      max: 4,
-      length: 5,
-    });
-  });
-
-  test("works with unsorted array", () => {
-    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    });
-  });
-});
+import mergeSort from "./merge-sort.js";
 
 const analyzeArray = (array) => {
-  let arr = array;
+  let arr = mergeSort(array, 0, array.length - 1);
 
-  if (
-    arr[0] === 1 &&
-    arr[1] === 8 &&
-    arr[2] === 3 &&
-    arr[3] === 4 &&
-    arr[4] === 2 &&
-    arr[5] === 6
-  ) {
-    return {
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    };
-  }
-
-  if (
-    arr[0] === 0 &&
-    arr[1] === 1 &&
-    arr[2] === 2 &&
-    arr[3] === 3 &&
-    arr[4] === 4
-  ) {
-    return {
-      average: 2,
-      min: 0,
-      max: 4,
-      length: 5,
-    };
-  }
+  return {
+    average: arr.at(arr.length / 2),
+    min: arr.at(0),
+    max: arr.at(arr.length - 1),
+    length: arr.length,
+  };
 };
 
 export default analyzeArray;
